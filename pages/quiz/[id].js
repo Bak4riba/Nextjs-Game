99772febae68367b0818f3556cfdd8c84ee2a0dd
Bakaus <matheus.bakaus.bruno@gmail.com>
2021-02-01 01:32:15 -0300

    buscando infos dbexterno

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -7,9 +7,24 @@ export default function QuizDaGaleraPage() {
     </div>
   );
 }
-export async function getServerSideProps(context) {
-  console.log('infos: ', context.query);
+/* export async function getServerSideProps(context) {
+
   return {
     props: '',
+  };
+} */
+export async function getServerSideProps(context) {
+  const dbExterno = await fetch('https://quiz-nextjs.bak4riba.vercel.app/api/db').then((responseServer) => {
+    if (responseServer.ok) {
+      return responseServer.json();
+    }
+    throw new Error('Falha em pegar os dados');
+  }).then((convert) => convert)
+    .catch((err) => {
+      console.log(err);
+    });
+    console.log(dbExterno)
+  return {
+    props: {}, // will be passed to the page component as props
   };
 }
