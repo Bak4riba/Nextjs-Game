b4ded45172f96c529eea31b345509626cbd0b6cb
Bakaus <matheus.bakaus.bruno@gmail.com>
2021-02-02 15:20:59 -0300

    METATAGS

--- pages/_app.js
+++ pages/_app.js
@@ -36,6 +36,39 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* METATAGS */}
+        {/* <!-- Primary Meta Tags --> */}
+        <title>Konoha Quiz!! Teste seus conhecimentos acerca de Naruto!!</title>
+        <meta name="title" content="Konoha Quiz!! Teste seus conhecimentos acerca de Naruto!!" />
+        <meta
+          name="description"
+          content="
+Quiz sobre o anime Naruto, desenvolvido na Imersão Alura - Next "
+        />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="Konoha Quiz!! Teste seus conhecimentos acerca de Naruto!!" />
+        <meta
+          property="og:description"
+          content="
+Quiz sobre o anime Naruto, desenvolvido na Imersão Alura - Next "
+        />
+        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:title" content="Konoha Quiz!! Teste seus conhecimentos acerca de Naruto!!" />
+        <meta
+          property="twitter:description"
+          content="
+Quiz sobre o anime Naruto, desenvolvido na Imersão Alura - Next "
+        />
+        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+
+        {/* FIM METATAGS */}
         <link rel="icon" href="https://i.ibb.co/Nj5YSwt/android-chrome-192x192.png" type="image/png" sizes="16x16" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
